da4940e157ba8d233595e34347849bf3bf365246
A-Cobra <conradbravina@gmail.com>
2024-02-27 15:54:57 -0500

    Created some styles for the nav bar

--- src/app/countries/components/sidebar/sidebar.component.ts
+++ src/app/countries/components/sidebar/sidebar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-sidebar',
@@ -6,6 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./sidebar.component.scss'],
 })
 export class SidebarComponent {
+  @Input() isMobileVersion = false;
+
   navigationRoutes = [
     { path: '/countries', name: 'Home' },
     { path: '/mock1', name: 'mock1' },
